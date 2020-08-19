3e532da4f9a974b652e6ab7f3af880b3cf9260fb
Abhay Kumar <abhay.kumar@geminisolutions.in>
2020-08-19 06:00:49 +0530

    heroku port config added

--- server.js
+++ server.js
@@ -22,6 +22,6 @@ if (process.env.NODE_ENV === "production") {
   );
 }
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Listening to PORT ${PORT}`));
